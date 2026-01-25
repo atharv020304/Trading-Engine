d240783c704c232bae47ffc8557f887e627cf570
Atharv Bapat <atharvabapat3@gmail.com>
2026-01-25 19:29:17 +0530

    updated OrderEventConsumer

--- src/main/java/com/exchange/trading/order/consumer/OrderEventConsumer.java
+++ src/main/java/com/exchange/trading/order/consumer/OrderEventConsumer.java
@@ -2,25 +2,32 @@ package com.exchange.trading.order.consumer;
 
 import com.exchange.trading.order.event.OrderEvent;
 import com.exchange.trading.order.service.OrderPersistenceService;
+import com.exchange.trading.orderbook.MatchResult;
+import com.exchange.trading.orderbook.MatchingEngine;
 import com.exchange.trading.orderbook.OrderBookService;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventConsumer {
     private final OrderBookService orderBookService;
     private final OrderPersistenceService orderPersistenceService;
-
-    public OrderEventConsumer(OrderBookService orderBookService, OrderPersistenceService orderPersistenceService) {
+    private final MatchingEngine matchingEngine;
+    public OrderEventConsumer(OrderBookService orderBookService, OrderPersistenceService orderPersistenceService, MatchingEngine matchingEngine) {
         this.orderBookService = orderBookService;
         this.orderPersistenceService = orderPersistenceService;
+        this.matchingEngine = matchingEngine;
     }
-
+    @KafkaListener(topics = "order-events", groupId= "orderbook-group")
     public void consume(OrderEvent event)
     {
-        System.out.println("Consuming OrderEvent: "+ event.getOrderId());
+    MatchResult result = matchingEngine.tryMatch(event);
 
+    if(result.isMatched()){
+        System.out.println("Matched order id: " + result.getMatchedOrderId());
+    }else {
         orderBookService.addOrder(event);
-
-        orderPersistenceService.save(event);
+    }
+    orderPersistenceService.save(event);
     }
 }
